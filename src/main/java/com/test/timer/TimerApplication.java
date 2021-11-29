0409cd2777224b129d4c9106f3c57b716f7a1263
libo <1197335635@qq.com>
2021-11-29 16:47:55 +0800

    读取本地配置 —> 读取nacos配置

--- src/main/java/com/test/timer/TimerApplication.java
+++ src/main/java/com/test/timer/TimerApplication.java
@@ -2,8 +2,12 @@ package com.test.timer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
 public class TimerApplication {
 
     public static void main(String[] args) {
